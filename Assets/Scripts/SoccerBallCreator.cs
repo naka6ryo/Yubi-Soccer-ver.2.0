81874974aff52704fa07c8b67533510067d51f2d
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-11-08 14:30:41 +0900

    試合開始、試合再開、ゴール時のSE再生実装

--- Assets/Scripts/SoccerBallCreator.cs
+++ Assets/Scripts/SoccerBallCreator.cs
@@ -8,11 +8,8 @@ public class SoccerBallCreator : MonoBehaviour
 {
     public string soccerPrefabName = "Soccer Ball";
     private GameObject localSoccerBallInstance;
-    private SoundManager soundManager;
     void Start()
     {
-        soundManager = SoundManager.Instance;
-        
         // 部屋に入っていない場合はスキップ
         if (!PhotonNetwork.IsConnected || !PhotonNetwork.InRoom)
         {
@@ -45,7 +42,6 @@ public class SoccerBallCreator : MonoBehaviour
 
         try
         {
-            soundManager.PlaySE("試合再開");
             localSoccerBallInstance = PhotonNetwork.Instantiate(soccerPrefabName, spawnPos, Quaternion.identity);
             if (localSoccerBallInstance != null)
             {
